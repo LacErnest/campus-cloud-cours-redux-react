0cd806354054536b54512cc8c5535ad0e29d940e
Laceinstein <laceinstein@gmail.com>
2022-08-30 17:07:43 +0200

    add all notes components

---
+++ src/components/AllNotes.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { removeNote } from '../actions/actions';
+
+class AllNotes extends Component {
+  removeNote = (index) => {
+    this.props.removeNote(index);
+  };
+
+  render() {
+    const notesItems = this.props.notes.map((note, index) => (
+      <li key={index}>
+        <b>{note.title}</b>
+        <button onClick={() => this.removeNote(index)}>x</button>
+        <br />
+        <span>{note.content}</span>
+      </li>
+    ));
+
+    return (
+      <React.Fragment>
+        <h3>All Notes</h3>
+
+        <ul>{notesItems}</ul>
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    notes: state.notes,
+  };
+};
+
+const mapDispatchToProps = {
+  removeNote: removeNote,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllNotes);
